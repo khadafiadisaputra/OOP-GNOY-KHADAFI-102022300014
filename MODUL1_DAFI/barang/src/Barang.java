a09504a7faf0a2df67457ff030f33dc6dedae1fb
khadafiadisaputra <khadafiadisaputra.10@gmail.com>
2024-10-14 08:22:25 +0700

    jurnal1

---
+++ MODUL1_DAFI/barang/src/Barang.java
@@ -0,0 +1,47 @@
+public class Barang {
+    //Atribut
+    private String nama;
+    private int jumlah;
+    private double harga;
+
+    // Constructor Barang
+    public Barang (String nama, int jumlah, double harga){
+        this.nama = nama;
+        this.jumlah = jumlah;
+        this.harga = harga;
+    }
+
+    // Getter dan Setter
+    public String getNama(){
+        return nama;
+    }
+    
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+    public int getJumlah(){
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah){
+        this.jumlah = jumlah;
+    }
+
+    public double getHarga(){
+        return harga;
+    }
+
+    public void setHarga(double harga){
+        this.harga = harga;
+    }
+
+
+    // Method tampilkanData
+    public void tampilkanData (){
+        System.out.println("Nama barang: "+ nama);
+        System.out.println("Jumlah barang: "+ jumlah);
+        System.out.println("Harga barang: "+ harga);
+    }
+    
+}
---
+++ MODUL1_DAFI/barang/src/ManajemenInventaris.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ManajemenInventaris {
+    // Deklarasi ArrayList untuk menyimpan barang
+    private ArrayList<String> daftarBarang = new ArrayList<>();
+    
+
+
+    // Method untuk menambah barang elektronik
+    public void tambahbarangelektronik(
+        // Scanner 
+
+        // Input Nama Barang, Jumlah Barang, Harga Barang, Garansi Barang
+
+        // Membuat objek barang elektronik baru
+
+
+    // Method untuk menambah barang non-elektronik
+
+        // Scanner
+
+
+        // Input Nama Barang, Jumlah Barang, Harga Barang, Material Barang
+
+        // Membuat objek barang non-elektronik baru
+
+
+    // Method untuk menampilkan semua barang memakai if-else (jika tidak ada barang akan menampilkan tidak ada barang dalam inventaris)
+
+}
